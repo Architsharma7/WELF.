9a4fc9a2a2a8d1f32a2c8be5d4f78ed3147d0a62
Dhruv-2003 <dhruvagarwal2017@gmail.com>
2023-02-19 18:19:25 +0530

    Proposal fetching added in the proposal page

--- frontend/front/pages/explore/[proposalID].js
+++ frontend/front/pages/explore/[proposalID].js
@@ -1,10 +1,64 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useRouter } from "next/router";
+import { useAccount, useContract, useProvider, useSigner } from "wagmi";
+import {
+  DAO_CONTRACT_ABI,
+  DAO_CONTRACT_ADDRESS,
+} from "../../constants/constants";
+import { ethers } from "ethers";
 
 const SpecificProposal = () => {
+  const [proposalID, setProposalID] = useState();
+  const [proposalData, setProposalData] = useState();
   const router = useRouter();
-  const proposalID = router.query.proposalID;
+
   // console.log(proposalID);
+
+  const { address, isConnected } = useAccount();
+  const provider = useProvider();
+
+  const DAO_Contract = useContract({
+    address: DAO_CONTRACT_ADDRESS,
+    abi: DAO_CONTRACT_ABI,
+    signerOrProvider: provider,
+  });
+
+  const fetchProposal = async (proposalId) => {
+    try {
+      /// Then fetch specific Data for the same
+      const data = await DAO_Contract.proposals(proposalId);
+      // console.log(data);
+      const proposalData = await (await fetch(data._infoCID)).json();
+      // console.log(proposalData);
+      const finalData = {
+        proposalID: proposalId,
+        title: proposalData.title,
+        description: proposalData.desc,
+        image: proposalData.imageCID,
+        video: proposalData.videoCID,
+        breakage: proposalData.donationbreakage,
+        amount: proposalData.donation,
+        duration: parseInt(data.duration),
+        startTime: parseInt(data.startTime),
+        fundContract: data.fundContract,
+        creator: data.creator,
+        verified: data.verified,
+        status: data.status,
+      };
+      // console.log(finalData);
+      setProposalData(finalData);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    const id = router.query.proposalID;
+    if (id) {
+      setProposalID(id);
+      fetchProposal(id);
+    }
+  }, [router.query.proposalID]);
   return (
     <div className="w-screen flex h-screen">
       <div className="mx-auto flex justify-center align-middle">
