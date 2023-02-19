74b71a5373dee2129140e6e3cea30fd4787e6b4d
Dhruv-2003 <dhruvagarwal2017@gmail.com>
2023-02-19 22:49:34 +0530

    AUTH completed

---
+++ frontend/front/auth/authContext.js
@@ -0,0 +1,81 @@
+import { storeProposal } from "../functions/ipfsstorage";
+import { useAccount, useContract, useProvider, useSigner } from "wagmi";
+import { NFT_CONTRACT_ABI, NFT_CONTRACT_ADDRESS } from "../constants/constants";
+import React, { useState, useEffect, createContext, useContext } from "react";
+import { useRouter } from "next/router";
+import { publicPaths } from "../constants/publicPath";
+
+const AuthContext = createContext();
+
+export const useAuth = () => {
+  return useContext(AuthContext);
+};
+
+export function AuthProvider({ children }) {
+  const router = useRouter();
+  const [isDAOMember, setIsDAOMember] = useState(false);
+  const [authorized, setAuthorized] = useState(false);
+  const { address, isConnected } = useAccount();
+  const provider = useProvider();
+  const { data: signer } = useSigner();
+
+  const NFT_Contract = useContract({
+    address: NFT_CONTRACT_ADDRESS,
+    abi: NFT_CONTRACT_ABI,
+    signerOrProvider: signer || provider,
+  });
+
+  const checkUser = async () => {
+    try {
+      const data = parseInt(await NFT_Contract.balanceOf(address));
+      if (data > 0) {
+        setIsDAOMember(true);
+      }
+      //   console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    checkUser();
+  }, [address]);
+
+  useEffect(() => {
+    const authCheck = () => {
+      // console.log(router);
+      if (
+        !isDAOMember &&
+        !publicPaths.includes(router.pathname.split("?")[0])
+      ) {
+        // console.log("NOT A PUBLIC ROUTE");
+        setAuthorized(false);
+        // dispatch(setRedirectLink({ goto: router.asPath }));
+        void router.push({
+          pathname: "/onboarding",
+        });
+      } else {
+        setAuthorized(true);
+      }
+    };
+
+    authCheck();
+
+    const preventAccess = () => setAuthorized(false);
+
+    router.events.on("routeChangeStart", preventAccess);
+    router.events.on("routeChangeComplete", authCheck);
+
+    return () => {
+      router.events.off("routeChangeStart", preventAccess);
+      router.events.off("routeChangeComplete", authCheck);
+    };
+  }, [router, router.events, address, isDAOMember]);
+
+  const value = {
+    isDAOMember,
+    authorized,
+  };
+
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+}
--- frontend/front/components/navbar.js
+++ frontend/front/components/navbar.js
@@ -1,8 +1,11 @@
 import { ConnectButton } from "@rainbow-me/rainbowkit";
-import React, {useState} from "react";
+import React, { useState } from "react";
+import { useAuth } from "../auth/authContext";
 
 const Navbar = () => {
-    const [open, setOpen] = useState(false);
+  const [open, setOpen] = useState(false);
+  const { isDAOMember } = useAuth();
+
   return (
     <div className="w-screen">
       <div className="xl:mx-auto xl:flex xl:justify-center xl:mt-5 mt-0">
@@ -10,7 +13,8 @@ const Navbar = () => {
           <div className="container flex flex-wrap items-center justify-between mx-auto">
             <a href="/" className="flex items-center">
               <span className="self-center text-2xl whitespace-nowrap text-black 4xl:text-4xl">
-                Welf<span className="text-2xl text-blue-600 4xl:text-4xl">.</span>
+                Welf
+                <span className="text-2xl text-blue-600 4xl:text-4xl">.</span>
               </span>
             </a>
             <div className="flex md:order-2">
@@ -20,15 +24,18 @@ const Navbar = () => {
               >
                 Connect Wallet
               </button> */}
-              <ConnectButton showBalance={false} accountStatus={{
-    smallScreen: 'avatar',
-    largeScreen: 'full',
-  }}/>
+              <ConnectButton
+                showBalance={false}
+                accountStatus={{
+                  smallScreen: "avatar",
+                  largeScreen: "full",
+                }}
+              />
               <button
                 className="inline-flex items-center p-2 text-sm text-black rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200"
                 aria-controls="navbar-sticky"
                 aria-expanded="false"
-                onClick={()=> setOpen(!open)}
+                onClick={() => setOpen(!open)}
               >
                 <span className="sr-only">Open main menu</span>
                 <svg
@@ -45,67 +52,138 @@ const Navbar = () => {
                 </svg>
               </button>
             </div>
-            {open &&
-               <div className="w-screen bg-white py-4 px-3 flex flex-col items-center mx-auto justify-center text-center">
-                <ul>
-                    <li className="text-black my-2"><a href="/">Home</a></li>
-                    <li className="text-black mt-2"><a href="/explore">Proposals</a></li>
-                    <li className="text-black my-2"><a href="/onboard">Join DAO</a></li>
-                    <li className="text-black my-2"><a href="/dashboard">Dashboard</a></li>
+            {open && (
+              <div className="w-screen bg-white py-4 px-3 flex flex-col items-center mx-auto justify-center text-center">
+                {isDAOMember ? (
+                  <ul>
+                    <li className="text-black my-2">
+                      <a href="/">Home</a>
+                    </li>
+                    <li className="text-black mt-2">
+                      <a href="/campaign">Campaigns</a>
+                    </li>
+                    <li className="text-black my-2">
+                      <a href="/explore">Proposals</a>
+                    </li>
+                    <li className="text-black my-2">
+                      <a href="/dashboard">Dashboard</a>
+                    </li>
+                  </ul>
+                ) : (
+                  <ul>
+                    <li className="text-black my-2">
+                      <a href="/">Home</a>
+                    </li>
+                    <li className="text-black mt-2">
+                      <a href="/campaign">Campaigns</a>
+                    </li>
+                    <li className="text-black my-2">
+                      <a href="/onboard">Join DAO</a>
+                    </li>
+                    <li className="text-black my-2">
+                      <a href="/donatordashboard">Dashboard</a>
+                    </li>
+                  </ul>
+                )}
+              </div>
+            )}
+            <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1">
+              {isDAOMember ? (
+                <ul className="flex flex-col p-4 mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0">
+                  <li>
+                    <a
+                      href="/"
+                      className="block py-2 pl-3 pr-4 text-black rounded md:bg-transparent md:p-0 text-xl relative before:content-[''] before:absolute before:block before:w-full before:h-[2px] 
+                    before:bottom-0 before:left-0 before:bg-black
+                    before:hover:scale-x-100 before:scale-x-0 before:origin-top-left
+                    before:transition before:ease-in-out before:duration-300 4xl:text-2xl"
+                    >
+                      Home
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="/campaign"
+                      className="block py-2 pl-3 pr-4 text-black rounded md:bg-transparent md:p-0 text-xl relative before:content-[''] before:absolute before:block before:w-full before:h-[2px] 
+                    before:bottom-0 before:left-0 before:bg-black
+                    before:hover:scale-x-100 before:scale-x-0 before:origin-top-left
+                    before:transition before:ease-in-out before:duration-300 4xl:text-2xl"
+                    >
+                      Campaigns
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="/explore"
+                      className="block py-2 pl-3 pr-4 text-black rounded md:bg-transparent md:p-0 text-xl relative before:content-[''] before:absolute before:block before:w-full before:h-[2px] 
+                    before:bottom-0 before:left-0 before:bg-black
+                    before:hover:scale-x-100 before:scale-x-0 before:origin-top-left
+                    before:transition before:ease-in-out before:duration-300 4xl:text-2xl"
+                    >
+                      Proposals
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="/dashboard"
+                      className="block py-2 pl-3 pr-4 text-black rounded md:bg-transparent md:p-0 text-xl relative before:content-[''] before:absolute before:block before:w-full before:h-[2px] 
+                    before:bottom-0 before:left-0 before:bg-black
+                    before:hover:scale-x-100 before:scale-x-0 before:origin-top-left
+                    before:transition before:ease-in-out before:duration-300 4xl:text-2xl"
+                    >
+                      Dashboard
+                    </a>
+                  </li>
                 </ul>
-               </div>
-        }
-            <div
-              className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
-            >
-              <ul className="flex flex-col p-4 mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0">
-                <li>
-                  <a
-                    href="/"
-                    className="block py-2 pl-3 pr-4 text-black rounded md:bg-transparent md:p-0 text-xl relative before:content-[''] before:absolute before:block before:w-full before:h-[2px] 
+              ) : (
+                <ul className="flex flex-col p-4 mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0">
+                  <li>
+                    <a
+                      href="/"
+                      className="block py-2 pl-3 pr-4 text-black rounded md:bg-transparent md:p-0 text-xl relative before:content-[''] before:absolute before:block before:w-full before:h-[2px] 
                     before:bottom-0 before:left-0 before:bg-black
                     before:hover:scale-x-100 before:scale-x-0 before:origin-top-left
                     before:transition before:ease-in-out before:duration-300 4xl:text-2xl"
-                  >
-                    Home
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="/explore"
-                    className="block py-2 pl-3 pr-4 text-black rounded md:bg-transparent md:p-0 text-xl relative before:content-[''] before:absolute before:block before:w-full before:h-[2px] 
+                    >
+                      Home
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="/campaign"
+                      className="block py-2 pl-3 pr-4 text-black rounded md:bg-transparent md:p-0 text-xl relative before:content-[''] before:absolute before:block before:w-full before:h-[2px] 
                     before:bottom-0 before:left-0 before:bg-black
                     before:hover:scale-x-100 before:scale-x-0 before:origin-top-left
                     before:transition before:ease-in-out before:duration-300 4xl:text-2xl"
-                  >
-                    Proposals
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="/onboarding"
-                    className="block py-2 pl-3 pr-4 text-black rounded md:bg-transparent md:p-0 text-xl relative before:content-[''] before:absolute before:block before:w-full before:h-[2px] 
+                    >
+                      Campaigns
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="/onboarding"
+                      className="block py-2 pl-3 pr-4 text-black rounded md:bg-transparent md:p-0 text-xl relative before:content-[''] before:absolute before:block before:w-full before:h-[2px] 
                     before:bottom-0 before:left-0 before:bg-black
                     before:hover:scale-x-100 before:scale-x-0 before:origin-top-left
                     before:transition before:ease-in-out before:duration-300 4xl:text-2xl"
-                  >
-                    Join DAO
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="/dashboard"
-                    className="block py-2 pl-3 pr-4 text-black rounded md:bg-transparent md:p-0 text-xl relative before:content-[''] before:absolute before:block before:w-full before:h-[2px] 
+                    >
+                      Join DAO
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="/donatordashboard"
+                      className="block py-2 pl-3 pr-4 text-black rounded md:bg-transparent md:p-0 text-xl relative before:content-[''] before:absolute before:block before:w-full before:h-[2px] 
                     before:bottom-0 before:left-0 before:bg-black
                     before:hover:scale-x-100 before:scale-x-0 before:origin-top-left
                     before:transition before:ease-in-out before:duration-300 4xl:text-2xl"
-                  >
-                    Dashboard
-                  </a>
-                </li>
-              </ul>
+                    >
+                      Dashboard
+                    </a>
+                  </li>
+                </ul>
+              )}
             </div>
-
           </div>
         </nav>
       </div>
